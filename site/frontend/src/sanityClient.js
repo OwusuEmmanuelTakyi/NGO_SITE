7fe090a5dd36a1cdcbe46521be145c396e8802c6
“OwusuEmmanuelTakyi” <owusuemmanueltakyi@gmail.com>
2024-09-14 01:28:06 +0000

    ..

--- site/frontend/src/sanityClient.js
+++ site/frontend/src/sanityClient.js
@@ -1,10 +1,10 @@
-import sanityClient from '@sanity/client';
+import { createClient } from '@sanity/client';
 require('dotenv').config();  // Load environment variables
 
-const client = sanityClient({
+const client = createClient({
   projectId: '08lyf801', // find this in your Sanity Studio dashboard
   dataset: 'production', // or whatever dataset you are using
   apiVersion: '2023-09-06', // use the latest API version
   useCdn: true, // `false` if you want to ensure fresh data
-  token: process.env.REACT_PUBLIC_SANITY_TOKEN
+  token: process.env.REACT_PUBLIC_SANITY_TOKEN // Use your environment variable here
 });
