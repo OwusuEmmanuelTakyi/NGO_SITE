e6280b6bf79d013e1bf04240ec5984a30cebfe31
“OwusuEmmanuelTakyi” <owusuemmanueltakyi@gmail.com>
2024-09-14 02:25:00 +0000

    .

--- site/frontend/src/sanityClient.js
+++ site/frontend/src/sanityClient.js
@@ -1,13 +1,12 @@
+// sanityClient.js or similar file
 import { createClient } from '@sanity/client';
-  // Load environment variables
-
-// Define the Sanity token outside the client config
-const sanityToken = process.env.REACT_APP_SANITY_TOKEN;
 
 const client = createClient({
   projectId: '08lyf801', // find this in your Sanity Studio dashboard
   dataset: 'production', // or whatever dataset you are using
   apiVersion: '2023-09-06', // use the latest API version
   useCdn: true, // `false` if you want to ensure fresh data
-  token: sanityToken // Use the token from environment variables
+  token: process.env.REACT_APP_SANITY_TOKEN // your environment variable
 });
+
+export default client;
