2e14e16bd266d96023ca0bfd70a4b93e48ee7c88
“OwusuEmmanuelTakyi” <owusuemmanueltakyi@gmail.com>
2024-09-14 01:15:29 +0000

    updated the sanityclient

--- site/frontend/src/sanityClient.js
+++ site/frontend/src/sanityClient.js
@@ -1,6 +1,7 @@
 import sanityClient from '@sanity/client';
+require('dotenv').config();  // Load environment variables
 
-export default sanityClient({
+export const client = sanityClient({
   projectId: '08lyf801', // find this in your Sanity Studio dashboard
   dataset: 'production', // or whatever dataset you are using
   apiVersion: '2023-09-06', // use the latest API version
